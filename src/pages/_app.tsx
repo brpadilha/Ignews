83a842b244f08388541cec38944d798e0f2f4d56
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-03-29 22:03:40 -0300

    estiliza o header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { AppProps } from "next/app"
 
 import "../styles/global.scss"
+import { Header } from '../components/Header/index';
 
 // AppProps é o tipo de props que o App recebe para as tipagens dos componentes
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
