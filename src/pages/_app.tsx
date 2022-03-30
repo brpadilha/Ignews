a26a7df2b5f12e31af3228da903bdd322e9c9b34
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-03-29 21:04:43 -0300

    configura estilo global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from "next/app"
 
+import "../styles/global.scss"
+
 // AppProps é o tipo de props que o App recebe para as tipagens dos componentes
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
