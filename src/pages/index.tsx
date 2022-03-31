9bb4b6a8d629986238bf0ce1d0b84431053ae284
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-03-31 15:23:36 -0300

    faz chamada no stripe para pegar dados via SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import SubscribeButton from '../components/SubscribeButton';
 import styles from './home.module.scss'
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+//props que recebemos aqui é o que vem do getServerSideProps
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,12 +26,45 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
+}
+
+// getServerSideProps é o método de fazer requisição no backend, ele é como o useEffect
+// vantagem de usar ele, é que os dados são carregados no servidor SSR antes de ser renderizado na tela
+// com isso ele é carregado já desde o começo da renderização da página
+export const getServerSideProps: GetServerSideProps = async () =>{
+  
+  // fazendo requisicao para pegar os dados do stripe
+  // priceId devemos pegar na pagina de produtos do stripe
+  const price = await stripe.prices.retrieve("price_1JDdDdCN91OfmpophCaRMSiG",{
+    // para ter acesso a todos os dados do protudo que está dentro do price, é como se fosse referencia para outra tabela
+    // se não vai trazer somente o id do produto
+    //expand: ["product"]
+  });
+
+  //dividido por 100 pq vem em centavos
+  const amountProdut = price.unit_amount / 100;
+
+  const product = {
+    priceId: price.id,
+    //IntL.NumberFormat é para formatar o valor para o dolar
+    amount: new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD'
+    }).format(amountProdut),
+
+  }
+
+  return {
+    props: {
+      product,
+    },
+  };
 }
