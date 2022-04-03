a7b621068721a31ffed715220a803de61716e23f
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-04-03 18:26:21 -0300

    make event more readable

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,12 +1,62 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Readable } from 'stream';
+import Stripe from 'stripe';
+import { stripe } from '../../services/stripe';
+
+// create strings of the stream and convert to a buffer
+// this is to be more readable in the code
+async function buffer(readable: Readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  }
+}
+
+const relevantEvents = new Set([
+  'checkout.session.completed',
+])
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default(req: NextApiRequest, res: NextApiResponse)=>{
-  console.log('evento recebido')
+export default async(req: NextApiRequest, res: NextApiResponse)=>{
 
-  res.status(200).json({
-    ok: true
-  })
+  if(req.method === 'POST'){
+    // transform the events to a readable string
+    const buff = await buffer(req);
+
+    const secret = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    try {
+      // verify if the stripe webhook is valid
+      event = stripe.webhooks.constructEvent(buff, secret, process.env.STRIPE_WEBHOOK_SECRET);
+    }catch(error){
+      return res.status(400).send(`Webhook Error: ${error.message}`);
+    }
+
+    const { type } = event;
+
+    if(relevantEvents.has(type)){
+       console.log('Evento recebido', event)
+    }
+
+    res.json({
+      received: true
+    })
+  }else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
 
